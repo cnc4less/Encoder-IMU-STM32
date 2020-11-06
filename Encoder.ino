c17f06683996ee636113808429d301284089b881
sirito97 <66971290+sirito97@users.noreply.github.com>
2020-11-07 00:08:23 +0200

    Update Encoder.ino

--- Encoder.ino
+++ Encoder.ino
@@ -3,35 +3,32 @@
 #define channelB PB12 
 volatile long encoderOutputValue = 0;
 int RPM = 0;
-// counters for the time to calculate the motor speed
 long pre_millis = 0 ;
 long cur_millis = 0 ;
 void setup() {
   Serial3.begin(115200);
   pinMode(channelA , INPUT_PULLUP);
-  pinMode(channelB , INPUT);
-  attachInterrupt(channelA, update_encoder, RISING);
+  pinMode(channelB , INPUT_PULLUP);
+  attachInterrupt(channelA, updateEncoderValue, RISING);
   pre_millis = millis() ;
 }
 
 void loop() {
     cur_millis = millis ();
-    if (cur_millis - pre_millis > 1000) {
-      RPM = (float)(encoderOutputValue / encoderOutputPerRotation);
-      Serial3.print("Motor speed= ");
-      Serial3.println(RPM);
-      encoderOutputValue = 0 ;
-      pre_millis = cur_millis ;
-    }
+  //  if (cur_millis - pre_millis > 1000) {
+  //    RPM = (float)(encoderOutputValue / encoderOutputPerRotation);
+  //    Serial3.print("Motor speed= ");
+  //    Serial3.println(RPM);
+  //    encoderOutputValue = 0 ;
+  //    pre_millis = cur_millis ;
+  //  }
   Serial3.println(encoderOutputValue);
 }
-void update_encoder () {
-  if (digitalRead(channelA) == HIGH){
+void updateEncoderValue () {
 	  if(digitalRead(channelB)==HIGH)
 	    encoderOutputValue++ ;
   	  else 
 	    encoderOutputValue -- ;
-   }
 }
 
 /*
