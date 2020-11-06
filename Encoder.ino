9d9f35d88702b63272a95345cffa40d3289f5497
sirito97 <66971290+sirito97@users.noreply.github.com>
2020-11-06 23:07:00 +0200

    Update Encoder.ino

--- Encoder.ino
+++ Encoder.ino
@@ -1,63 +1,50 @@
-
-// Motor encoder output pulse per rotation (change as required)
-#define ENC_COUNT_REV 35
-
-// Encoder output to Arduino Interrupt pin
-#define ENC_IN 3 
-
-
-// Pulse count from encoder
-volatile long encoderValue = 0;
-
-// One-second interval for measurements
-int interval = 1000;
-
-// Counters for milliseconds during interval
-long previousMillis = 0;
-long currentMillis = 0;
-
-// Variable for RPM measuerment
-int rpm = 0;
-
-
-
-void setup()
-{
-  // Setup Serial Monitor
-  Serial.begin(9600); 
-  
-  // Set encoder as input with internal pullup  
-  pinMode(ENC_IN, INPUT_PULLUP); 
-
-  // Attach interrupt 
-  attachInterrupt(digitalPinToInterrupt(ENC_IN), updateEncoder, RISING);
-  
-  // Setup initial values for timer
-  previousMillis = millis();
+#define encoderOutputPerRotation 1024 
+#define channelA PB13 
+#define channelB PB12 
+volatile long encoderOutputValue = 0;
+int RPM = 0;
+// counters for the time to calculate the motor speed
+long pre_millis = 0 ;
+long cur_millis = 0 ;
+void setup() {
+  Serial3.begin(115200);
+  pinMode(channelA , INPUT_PULLUP);
+  pinMode(channelB , INPUT);
+  attachInterrupt(channelA, update_encoder, RISING);
+  pre_millis = millis() ;
 }
 
-void loop()
-{
- // Serial.println( encoderValue);
-  
-
-  // Update RPM value every second
-  currentMillis = millis();
-  if (currentMillis - previousMillis > interval) {
-    previousMillis = currentMillis;
-
-
-    // Calculate RPM
-    rpm = (float)(encoderValue * 60 / ENC_COUNT_REV);
-	Serial.println(rpm);
-    encoderValue=0;
-
-    
-  }
+void loop() {
+    cur_millis = millis ();
+    if (cur_millis - pre_millis > 1000) {
+      RPM = (float)(encoderOutputValue / encoderOutputPerRotation);
+      Serial3.print("Motor speed= ");
+      Serial3.println(RPM);
+      encoderOutputValue = 0 ;
+      pre_millis = cur_millis ;
+    }
+  Serial3.println(encoderOutputValue);
+}
+void update_encoder () {
+  if (digitalRead(channelA) == HIGH){
+	  if(digitalRead(channelB)==HIGH)
+	    encoderOutputValue++ ;
+  	  else 
+	    encoderOutputValue -- ;
+   }
 }
 
-void updateEncoder()
-{
-  // Increment value for each pulse from encoder
-  encoderValue++;
-}
+/*
+clockwise
+when A on Rising state B is High
+     ____      ____      ____      ____ 
+____|    |____|    |____|    |____|    |  (((A)))
+   ____      ____      ____      ____ 
+__|    |____|    |____|    |____|    |    (((B)))
+counterclockwise
+when A on Rising state B is LOw
+   ____      ____      ____      ____ 
+__|    |____|    |____|    |____|    |    (((A)))
+     ____      ____      ____      ____ 
+____|    |____|    |____|    |____|    |  (((B)))
+*/
